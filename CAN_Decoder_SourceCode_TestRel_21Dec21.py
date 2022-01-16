ff953fbf7ca99d17e32bd675813582973abdaddb
Kiran Kumar T <96540532+KiranQode@users.noreply.github.com>
2022-01-16 21:57:47 +0530

    Revised code as per our discussion. Please check.

--- CAN_Decoder_SourceCode_TestRel_21Dec21.py
+++ CAN_Decoder_SourceCode_TestRel_21Dec21.py
@@ -2,96 +2,108 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 from datetime import datetime
+import re
 
 
-# Function for opening the file
 def CAN_Decoder():
-    lineNum = 0
-    oldDataFile = filedialog.askopenfile(initialdir="/")
-    listedOldData = []
-    for dataLine in oldDataFile:
-        segmentedDataLine = dataLine.split(" ")
-        listedOldDataLine = []
-        for stringSegment in segmentedDataLine:
-            if stringSegment.strip():
-                listedOldDataLine.append(stringSegment)
-        listedOldData.append(listedOldDataLine)
-        lineNum = lineNum + 1
-
-    newData = []
-    lineCount = len(listedOldData)
-    for dataLineIndex in range(0, lineCount):
-        dataLine = listedOldData[dataLineIndex]
-        refinedDataLine = []
-        frameType = int(dataLine[3])
-
-        # SingleFrame
+    dataFile = filedialog.askopenfilename()
+    inputFrameData = []
+    
+    # Extract frame from text file using regex approach
+    typicalFrameForm = r"can(\d)\s+([(\w)]{1,3})\s+\[(\d+)\]\s+([\w]{2}(?:\s[\w]{2})*)"
+    frameSize = 0
+    for dataFrame in re.findall(typicalFrameForm, open(dataFile).read()):
+        diagnosticID = int(dataFrame[1], 16)
+        data = list(map(lambda x: int(x, 16), dataFrame[3].split(" ")))
+        frameType = data[0]
         if frameType <= 6:
-            refinedDataLine.append(dataLine[1])
-            index = 4
-            for i in range(index, index + frameType):
-                refinedDataLine.append(dataLine[i])
-            p = ' '.join(refinedDataLine)
-            newData.append(p)
-
-        # Multi frame
-        elif frameType == 10:
-            # First frame data
-            diagnosticID = dataLine[1]
-            frameSize = int(dataLine[4], 16)
-            refinedDataLine.append(dataLine[1])
-            index = 5
-            eleCount = 0
-            for j in range(index, len(dataLine)):
-                refinedDataLine.append(dataLine[j])
-                eleCount += 1
-
-            # Add consecutive frame data
-            startLineIndex = dataLineIndex + 1
-            index = 4
-            seqCount = -1
-            startSeqNum = 21
-            for i in range(startLineIndex, lineCount):
-                dataLineCF = listedOldData[i]
-                CFdiagnosticID = dataLineCF[1]
-                if diagnosticID == CFdiagnosticID:
-                    seqNum = int(dataLineCF[3])
-                    if seqNum != 10:
-                        seqCount += 1
-                        if seqNum == startSeqNum + seqCount:
-                            for j in range(index, len(dataLineCF)):
-                                if eleCount <= frameSize:
-                                    refinedDataLine.append(dataLineCF[j])
-                                    eleCount += 1
-                        #else:
-                            #print('Sequence Number mismatch found: ', seqNum, seqCount)
-                    else:
-                        break
-            temp = []
-            for element in refinedDataLine:
-                temp.append(element.strip())
-            refinedDataLine = temp
-            p = ' '.join(refinedDataLine)
-            newData.append(p)
-
-        # Flow control frame
-        #elif frameType == 30:
-            # Do nothing
-            #newData.append('FCF')
-
-        # Consecutive frame
-        #else:
-            # Do nothing
-            #newData.append('CF')
+            frameSize = frameType  # Single frame
+        elif frameType == 16:  # 10 for hex notation
+            frameSize = data[1]
+        frameMap = {"DiagnosticID": diagnosticID,
+                    "FrameType": frameType,
+                    "FrameSize": frameSize,
+                    "PayloadData": data}
+        inputFrameData.append(frameMap)
+    extractedData = CAN_ReAssembly(inputFrameData)
 
     # Output file
     dateTime = datetime.today().strftime('_%Y%m%d_%H%M%S')
-    outputFileName = oldDataFile.name.split(".", 1)[0] + dateTime + '_Output.txt'
+    outputFileName = dataFile.split(".", 1)[0] + dateTime + '_Output.txt'
     decodedTextFile = open(outputFileName, "w")
-    for element in newData:
-        decodedTextFile.write(element + "\n")
+    for element in extractedData:
+        for item in element:
+            decodedTextFile.write(str(item) + " ")
+        decodedTextFile.write("\n")
     decodedTextFile.close()
-    print('Decoded file generated. Please check at original file location.')
+    print('Decoded file generated. Please check at input file location.')
+
+
+def CAN_ReAssembly(formattedRawData):
+    extractedData = []
+    lineCount = len(formattedRawData)
+    inputData_dict = {}
+    newLineCount = 0
+    cFrameCount = 0
+    for dataLineIndex in range(0, lineCount):
+        dataLine = formattedRawData[dataLineIndex]
+        frameType = dataLine["FrameType"]
+        diagnosticId = dataLine["DiagnosticID"]
+        frameSize = dataLine["FrameSize"]
+        if frameType <= 6:      # Single frame
+            sFrameIndex = 1     # Single frame index
+            payloadData = []
+            for i in range(sFrameIndex, sFrameIndex + frameSize):
+                payloadData.append(dataLine["PayloadData"][i])
+            frameMap = {"DiagnosticID": diagnosticId,
+                        "FrameType": frameType,
+                        "FrameSize": frameSize,
+                        "PayloadData": payloadData}
+            newLineCount += 1
+            inputData_dict[str(newLineCount)] = frameMap
+        elif frameType == 16:   # 10 for hex notation | Continuous frame
+            frameSize = dataLine["FrameSize"]
+            eleCount = 0
+            fFrameIndex = 2     # First frame index
+            payloadData = []
+            for j in range(fFrameIndex, len(dataLine["PayloadData"])):
+                payloadData.append(dataLine["PayloadData"][j])
+                eleCount += 1
+            startLineIndex = dataLineIndex + 1
+            cFrameIndex = 1     # Consecutive frame index
+            seqCount = -1
+            startSeqNum = 33    # 21 for hex notation
+            for i in range(startLineIndex, lineCount):
+                dataLine_CF = formattedRawData[i]
+                diagnosticID_CF = dataLine_CF["DiagnosticID"]
+                if diagnosticId == diagnosticID_CF:
+                    seqNum = dataLine_CF["PayloadData"][0]
+                    if seqNum != 16:   # 10 for hex notation
+                        seqCount += 1
+                        if seqNum == startSeqNum + seqCount:
+                            for j in range(cFrameIndex, len(dataLine_CF["PayloadData"])):
+                                if eleCount <= frameSize:
+                                    payloadData.append(dataLine_CF["PayloadData"][j])
+                                    eleCount += 1
+                    else:
+                        break
+            frameMap = {"DiagnosticID": diagnosticId,
+                        "FrameType": frameType,
+                        "FrameSize": frameSize,
+                        "PayloadData": payloadData}
+            newLineCount += 1
+            inputData_dict[str(newLineCount)] = frameMap
+
+        elif frameType > 32 and frameType >= 48:   # 20 & 30 for hex notation
+            cFrameCount += 1     # No action required
+    print(str(cFrameCount) + " consecutive frames found")
+
+    for key, value in inputData_dict.items():
+        newDataLine = [value["DiagnosticID"]]
+        for item in value["PayloadData"]:
+            newDataLine.append(item)
+        extractedData.append(newDataLine)
+    return extractedData
 
 
 # Create UI
@@ -100,7 +112,7 @@ parent.title('CAN Decoder')
 parent.geometry('300x50')
 frame = tk.Frame(parent)
 frame.pack()
-fileButton = tk.Button(frame, text="Select CANData file(.txt)", fg="Green", command=lambda: CAN_Decoder())
+fileButton = tk.Button(frame, text="Select CANData file(.txt)", fg="Green", command=CAN_Decoder())
 fileButton.pack(side=LEFT)
 exit_button = tk.Button(frame, text="Exit", fg="Red", command=quit)
 exit_button.pack(side=RIGHT)
